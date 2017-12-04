d0899885f83d317a844a02e94d62a3f6f7c1321c
Markus Felten <Markus.Felten@gmx.de>
2017-12-04 23:32:41 +0100

    chore: avarization

--- src/obj2string.js
+++ src/obj2string.js
@@ -1,54 +1,38 @@
-/* jslint node: true, esnext: true */
-'use strict';
-
 /**
  * Transforms an object stream into a stream of strings
  */
 
-const stream = require('stream');
+const { Transform } = require('stream');
 
-class Obj2String extends stream.Transform {
+export class Obj2String extends Transform {
+  /**
+   * Creates the line parser and sets the options.
+   * The following options are supported:
+   * {
+   *	"allow_new_line_in_cell" : true,
+   * 	"line_separator" : "\n",
+   *  "quote_char" : '"'
+   *  "skip_empty_lines" : true
+   * }
+   *
+   */
+  constructor(opts = {}) {
+    super({
+      objectMode: true,
+      highWaterMark: 16
+    });
+  }
 
-	/**
-	 * Creates the line parser and sets the options.
-	 * The following options are supported:
-	 * {
-	 *	"allow_new_line_in_cell" : true,
-	 * 	"line_separator" : "\n",
-	 *  "quote_char" : '"'
-	 *  "skip_empty_lines" : true
-	 * }
-	 *
-	 */
-	constructor(opts) {
-		// call the constrctor of stream.Transform
-		super({
-			objectMode: true,
-			highWaterMark: 16
-		});
+  /**
+   * Reads the stream data and split it into lines.
+   */
+  _transform(data, enc, next) {
+    // if the data type is a string, convert it to a Buffer
+    if (typeof data !== 'string') {
+      data = JSON.stringify(data) + '\n';
+    }
 
-		if (!opts) opts = {};
-	}
-
-	/**
-	 * Reads the stream data and split it into lines.
-	 */
-	_transform(data, enc, next) {
-			// if the data type is a string, convert it to a Buffer
-			if (typeof data !== 'string') {
-				data = JSON.stringify(data) + "\n";
-			}
-
-			this.push(data);
-			next();
-		} // end transform
+    this.push(data);
+    next();
+  }
 }
-
-function Obj2StringFactory(opts) {
-	return new Obj2String(opts);
-}
-
-
-export {
-	Obj2StringFactory
-};
